2541b31224c4e3a2b3c001680ebafc25fbfb7e3c
Catsama1337 <haianhvn113.namdinh@gmail.com>
2025-09-23 11:20:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ function test() {
 }
 
 
-let toggle = true;
+let toggle = false;
 
 
 function anime() {
       const img = document.getElementById("myImage");
-        if(toggle) {img.src = "https://res.cloudinary.com/dyqfa69ac/image/upload/v1758197787/sddefault_l7kpqf.jpg";
+        if(!toggle) {img.src = "https://res.cloudinary.com/dyqfa69ac/image/upload/v1758197787/sddefault_l7kpqf.jpg";
     
 } else {
   img.src ="https://res.cloudinary.com/dyqfa69ac/image/upload/v1751284003/IMG_20250630_111128_sidnfa.jpg"
@@ -25,3 +25,4 @@ function anime() {
   
   toggle = !toggle;
 }
+
